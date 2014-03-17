3c512455f2581698441389075bf74d648023d4b8
central <central@think.yak.net>
2014-03-16 20:34:13 -0700

    gofmt

--- coded/main.go
+++ coded/main.go
@@ -9,7 +9,7 @@
 
 	go build coded/main.go && for s in 1 2 3 4 5 6 ; do time ./main  -o _e$s.png -p 'g;f' -n 5000000 -s $s -d 18 -fuzz=1 -w=5000 -h=5000 ; done
 
-	 go build coded/main.go && for s in 1 2 3 4 5 6 ; do time ./main  -o _e$s.png -p 'f;f;g' -f 4 -n 1000000 -s $s -d 14 -fuzz=1 -w=2000 -h=2000 ; done 
+	 go build coded/main.go && for s in 1 2 3 4 5 6 ; do time ./main  -o _e$s.png -p 'f;f;g' -f 4 -n 1000000 -s $s -d 14 -fuzz=1 -w=2000 -h=2000 ; done
 
 
 */
@@ -26,16 +26,16 @@ import (
 )
 
 var (
-	filename      = flag.String("o", "_", "basename for output images")
+	filename = flag.String("o", "_", "basename for output images")
 	seed     = flag.Int("s", 1, "seed for code tree")
-	fanout    = flag.Int("f", 3, "depth of code tree")
-	depth     = flag.Int("d", 12, "depth of code tree")
-	num       = flag.Int("n", 1000, "number if iterations")
-	width     = flag.Int("w", 1000, "width in pixels")
-	height    = flag.Int("h", 1000, "width in pixels")
-	params    = flag.String("p", "", "IFS parameters, as lists of matrices, or name of a Builtin IFS.")
-	list      = flag.Bool("l", false, "Just list short names for builtin IFSs.")
-	fuzz      = flag.Int("fuzz", 1, "Fuzzy Point Size")
+	fanout   = flag.Int("f", 3, "depth of code tree")
+	depth    = flag.Int("d", 12, "depth of code tree")
+	num      = flag.Int("n", 1000, "number if iterations")
+	width    = flag.Int("w", 1000, "width in pixels")
+	height   = flag.Int("h", 1000, "width in pixels")
+	params   = flag.String("p", "", "IFS parameters, as lists of matrices, or name of a Builtin IFS.")
+	list     = flag.Bool("l", false, "Just list short names for builtin IFSs.")
+	fuzz     = flag.Int("fuzz", 1, "Fuzzy Point Size")
 )
 
 func main() {
@@ -52,38 +52,38 @@ func main() {
 	// Parse IFSs & construct initial Triptych.
 	ifss := superfractal.ParseListOfIfsParams(*params)
 	tree := &superfractal.CodeTree{
-		Depth: *depth,
+		Depth:  *depth,
 		Fanout: *fanout,
-		IFSs: ifss,
+		IFSs:   ifss,
 	}
 	tree.Init(*seed)
 
 	can := canvas.NewCanvas(*width, *height)
 	tmp1 := make([]byte, *depth)
 	tmp2 := make([]byte, *depth)
-	for i:=0; i<*num; i++ {
+	for i := 0; i < *num; i++ {
 		x, y := tree.RandomPoint(tmp1, tmp2)
 
 		if *fuzz > 1 {
 			xf := rand.Intn(*fuzz)
 			yf := rand.Intn(*fuzz)
-			can.FSet(x + float64(xf)/float64(*width), y + float64(yf)/float64(*height), canvas.White)
+			can.FSet(x+float64(xf)/float64(*width), y+float64(yf)/float64(*height), canvas.White)
 		} else {
 			can.FSet(x, y, canvas.White)
 		}
-/*
-		for xf := 0; xf < *fuzz; xf++ {
-		for yf := 0; yf < *fuzz; yf++ {
-			can.FSet(x + float64(xf)/float64(*width), y + float64(yf)/float64(*height), canvas.White)
-		}
-		}
-*/
+		/*
+			for xf := 0; xf < *fuzz; xf++ {
+			for yf := 0; yf < *fuzz; yf++ {
+				can.FSet(x + float64(xf)/float64(*width), y + float64(yf)/float64(*height), canvas.White)
+			}
+			}
+		*/
 	}
 
 	canvas.Say(*filename)
 	fd, err := os.Create(*filename)
 	if err != nil {
-				panic(err)
+		panic(err)
 	}
 	can.WritePng(fd)
 	fd.Close()
--- codetree.go
+++ codetree.go
@@ -11,12 +11,12 @@ import (
 
 type CodeTree struct {
 	// Set on creation:
-	Depth	int
-	Fanout	int
-	IFSs	[]*IFS
+	Depth  int
+	Fanout int
+	IFSs   []*IFS
 
 	// Computed by Init:
-	Codes	[][]byte
+	Codes [][]byte
 }
 
 func (o *CodeTree) Init(seed int) {
@@ -24,7 +24,7 @@ func (o *CodeTree) Init(seed int) {
 	r := rand.New(src)
 	o.Codes = make([][]byte, o.Depth)
 	pow := 1
-	for i:=0; i<o.Depth; i++ {
+	for i := 0; i < o.Depth; i++ {
 		o.Codes[i] = make([]byte, pow)
 		for j, _ := range o.Codes[i] {
 			o.Codes[i][j] = byte(r.Int31())
@@ -46,15 +46,15 @@ func (o *CodeTree) LookupPath(path []byte, out []byte) {
 func (o *CodeTree) TransformPath(path []byte, choices []byte) (float64, float64) {
 	n := len(o.IFSs)
 	var x, y float64
-/*
-	for i := len(path) - 1; i >= 0; i-- {
-		ifs := o.IFSs[int(choices[i]) % n]
-		fn := ifs.Choices[path[i]]
-		x, y = fn.Apply(x, y)
-	}
-*/
+	/*
+		for i := len(path) - 1; i >= 0; i-- {
+			ifs := o.IFSs[int(choices[i]) % n]
+			fn := ifs.Choices[path[i]]
+			x, y = fn.Apply(x, y)
+		}
+	*/
 	for i := len(path) - 1; i > 0; i-- {
-		ifs := o.IFSs[int(choices[i-1]) % n]
+		ifs := o.IFSs[int(choices[i-1])%n]
 		fn := ifs.Choices[path[i]]
 		x, y = fn.Apply(x, y)
 	}
@@ -65,7 +65,7 @@ func (o *CodeTree) TransformPath(path []byte, choices []byte) (float64, float64)
 func (o *CodeTree) RandomPoint(tmp1, tmp2 []byte) (float64, float64) {
 	// Choose the path.
 	n := len(tmp1)
-	for i:=0; i<n; i++ {
+	for i := 0; i < n; i++ {
 		tmp1[i] = byte(rand.Intn(o.Fanout))
 	}
 
